b154c324f1c6ddacc659edc82d65a66163b72fa9
Diego García <diegogarciag63@gmail.com>
2022-03-24 16:58:14 -0500

    Clases y herencia con clases

--- CaracterísticasJS/POO/herency.js
+++ CaracterísticasJS/POO/herency.js
@@ -1,3 +1,6 @@
+
+// HERENCIA PROTOTIPICA
+
 function Animal(nombre, genero) {
 
     this.nombre = nombre;
@@ -13,3 +16,47 @@ Animal.prototype.sonar = function (){
 Animal.prototype.saludar = function () {
     console.log(`Hola, mi nombre es ${this.nombre}`)
 }
+
+
+function Perro(nombre, genero, size, raza){
+
+    // Para hacer la herencia de las caracteristicas que ya se encuentran en el constructor "Animal" necesitamos
+    // generar un super constructor que nos herede las dichas caracteristicas
+    // (En este caso son "nombre" y "genero ") 
+
+    this.super = Animal; // Aqui estamos diciendo que en el elemento super, su padre sera la funcion Animal
+    this.super(nombre, genero); // Luego debemos ejecutar el metodo super 
+
+    this.size = size;
+    this.raza = raza;
+
+
+
+}
+
+// Hacer que perro herede directamente de Animal =>
+
+Perro.prototype = new Animal();
+Perro.prototype.constructor = Perro;    
+
+// Sobreescritura de metodos del prototipo padre en el hijo
+
+Perro.prototype.sonar = function(){
+    console.log("Soy un perro y mi sonido es un ladrido")
+
+}
+
+Perro.prototype.ladrar = function(){
+    console.log("Guau Guau !!!!")
+
+}
+
+const Snoopy = new Perro ("Snoopy", "Macho", "Grande", "Dalmata")
+
+console.log(Snoopy)
+
+Snoopy.sonar();
+Snoopy.ladrar();
+Snoopy.saludar();
+
+// HERENCIA CON CLASES
