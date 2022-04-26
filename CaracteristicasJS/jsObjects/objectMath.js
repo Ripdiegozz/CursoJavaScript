5aeeff92ccd219ba3a8c74ed5679cb3e7347331d
Diego García <diegogarciag63@gmail.com>
2022-04-26 00:09:19 -0500

    Object Math

---
+++ CaracteristicasJS/jsObjects/objectMath.js
@@ -0,0 +1,29 @@
+
+console.log(Math);
+
+console.log(Math.PI); // El objeto math tiene metodos en mayuscula porque son constantes matematicas
+
+console.log(Math.abs(-9.6)); // Revela el valor absoluto de un numero
+
+console.log(Math.ceil(7.6)); // Redondea un numero al valor superior
+console.log(Math.ceil(7.2)); 
+
+console.log(Math.floor(7.6)); // Redondea un numero al valor inferior
+console.log(Math.floor(7.2));
+
+console.log(Math.round(7.6)); // Redondea al numero cercano mas inmediato
+console.log(Math.round(7.2));
+
+console.log(Math.sqrt(81)); // Revelar raiz cuadrada
+
+console.log(Math.pow(4,4)); // Potenciar un numero (El primer numero es la base, el segundo el exponente)
+
+console.log(Math.sign(-3)); // -1 Indica numero negativo
+console.log(Math.sign(0));  // 0 Indica numero 0
+console.log(Math.sign(3));  // 1 Indica numero positivo
+
+
+console.log(Math.random()); // Nos devuelve un numero aleatorio entre 0 y 1
+console.log(Math.random() * 100); // Nos devuelve un numero aleatorio entre 0 y 100
+console.log(Math.random() * 1000); // Nos devuelve un numero aleatorio entre 0 y 1000 
+
