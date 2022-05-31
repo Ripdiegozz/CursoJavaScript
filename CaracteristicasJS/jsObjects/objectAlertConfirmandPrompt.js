6b8d3064e20d3ab256961f795786ab3c396b8ee1
DiegoGarcia <diegogarciag63@gmail.com>
2022-05-31 17:20:18 -0500

    Ventanas intrusivas

---
+++ CaracteristicasJS/jsObjects/objectAlertConfirmandPrompt.js
@@ -0,0 +1,15 @@
+console.log(window);
+
+// alert('Hola esto es una alerta')
+// confirm('Hola esto es una confirmacion')
+// prompt('Hola esto es un aviso y le permite al usuario ingresar un valor')
+
+let alerta = alert('Esto es una alerta');
+let confirmacion = confirm('Esto es una confirmacion');
+let aviso = prompt('Esto es un prompt y le permite al usuario ingresar algun valor');
+
+console.log(alerta);
+console.log(confirmacion);
+console.log(aviso);
+
+
