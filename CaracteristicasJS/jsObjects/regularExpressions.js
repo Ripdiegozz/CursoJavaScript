0444f79a4468f7f9332cd983dd431893871eb12c
DiegoGarcia <diegogarciag63@gmail.com>
2022-06-10 22:22:00 -0500

    Se eliminan los archivos txt probados y se completa el archivo sobre la clase de regular expressions

--- CaracteristicasJS/jsObjects/regularExpressions.js
+++ CaracteristicasJS/jsObjects/regularExpressions.js
@@ -3,6 +3,7 @@
 Expresiones Regulares
 
 https://es.wikipedia.org/wiki/Expresi%C3%B3n_regular
+
 https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions
 
 Son una secuencia de caracteres que forma un patron de busqueda, 
@@ -11,13 +12,41 @@ de cadenas de caracteres
 
 */
 
-alert('Programa para validar si se inserto Cedula de Ciudadania');
+let cadena = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet massa sed turpis 
+              viverra porttitor. Ut consequat felis eu libero viverra, at ultricies magna gravida. 
+              Etiam sed dolor quis lacus temporlorem posuere vitae sit amet elit. Aliquam pellentesque 
+              eget velit nec tincidunt. Phasellus feugiat vitae orci eget venenatis. Nulla quis faucibus
+              metus. Ut vitae velit sagittis, convallis enimlorem a, consectetur magna. Vivamus elementum ipsum
+              nibh, ut sollicitudin neque auctor eget.`;
 
-let ccDiego = prompt('Ingresa tu Cedula de Ciudadania');
 
-console.log(ccDiego);
+/* 
+   la forma de declarar expresiones regulares es con dos parametros
+   new RegExp('cadena que vamos a testear', 'banderas que vamos a agregar')
+   
+   banderas importantes: 
+
+   i -> ignora mayusculas y minusculas
+   g -> no se detiene en la primera coincidencia, cuenta todas las coincidencias de la expresion regular
+*/              
+
+/*
+   rangos en expresiones regulares:
+    
+   {} -> para dar una probabilidad. ej: {2, 3} // esto quiere decir que el conjunto de caracteres se puede repetir
+                                                  de dos a tres veces
 
 
-let expReg = new RegExp(ccDiego, '^((\d{8})|(\d{10})|(\d{11})|(\d{6}-\d{5}))?$')
+*/
 
-console.log(expReg.test(ccDiego))
+let expReg = new RegExp('lorem','ig');
+
+let expReg2 = /lorem/ig;
+
+
+//console.log(expReg.test(cadena));
+//console.log(expReg.exec(cadena));
+
+
+console.log(expReg2.test(cadena));
+console.log(expReg2.exec(cadena));
